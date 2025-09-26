3e7efa91a9a90ef8da8ca11aa6c10d9bdcead701
hijhye <yesyuji17@gmail.com>
2025-09-26 16:08:47 +0900

    2

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,12 @@ $(function () {
     let isOn = $(this).siblings(".detail").hasClass("on");
 
     if (isOn) {
+      $(this).removeClass("on");
       $(this).siblings(".detail").removeClass("on");
     } else {
       $(".contentWrap .detail").removeClass("on");
+      $("contentWrap .title").removeClass("on");
+      $(this).addClass("on");
       $(this).siblings(".detail").addClass("on");
     }
   });
